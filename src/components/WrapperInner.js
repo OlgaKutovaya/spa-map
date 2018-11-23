fcb5ac228a30ebebfb80079322432c039918b401
OlgaKutovaya <o.kutovaya.ak@gmail.com>
2018-11-23 09:44:49 +0300

    Added function to display information about selected path

--- src/components/WrapperInner.js
+++ src/components/WrapperInner.js
@@ -1,19 +1,45 @@
  import React from 'react';
  import PathListBox from './PathListBox';
+ import {connect} from 'react-redux';
 
- const WrapperInner = () => {
+ const WrapperInner = (props) => {
+     let activeItem = null;
+
+     const activeItemIndex = props.path.paths.findIndex((item) => {
+         return item.isActive
+     });
+     // console.log(activeItemIndex);
+     if (activeItemIndex > -1) {
+         activeItem = props.path.paths[activeItemIndex];
+     }
      return (
-         <main className="row">
+         <main className="row main-content-wrapper">
              {/*leftside*/}
              <div className='col'>
-                 <PathListBox />
+                     <PathListBox />
              </div>
              {/*rightside*/}
-             <div className='col'>
+             <div className='col map-api'>
+                 {activeItem ? (<div className="map-api-item">
+                    <div>
+                        {activeItem.name}
+                        {activeItem.fullDescription}
+                    </div>
+                 </div>) : (
+                     <div className="map-no-active-item">
 
+                     </div>
+                 )}
              </div>
          </main>
      )
  };
 
- export default WrapperInner;
+
+ const mapStateToProps = (state) => {
+     return {
+         path: state.path
+     }
+ };
+
+ export default connect(mapStateToProps, {}) (WrapperInner);
